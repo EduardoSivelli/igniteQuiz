3fae7702c59789ebc939e6beaecbaf062f51dd74
EduardoSivelli <marquessivelli@hotmail.com>
2024-07-15 16:13:20 -0300

    finishing project

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
 import { Routes } from './src/routes';
 
 export default function App() {
@@ -14,14 +17,14 @@ export default function App() {
   }
   
   return (
-    <>
+    <GestureHandlerRootView style={{flex: 1}}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </GestureHandlerRootView>
   );
 }
 
--- src/screens/History/styles.ts
+++ src/screens/History/styles.ts
@@ -8,6 +8,23 @@ export const styles = StyleSheet.create({
     backgroundColor: THEME.COLORS.GREY_800,
   },
   history: {
+    flexGrow: 1,
     padding: 32,
-  }
+  },
+  swipeableRemove: {
+    width: "100%",
+    height: 90,
+    borderRadius: 6,
+    marginLeft: 30,
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    alignItems: 'flex-start',
+    justifyContent:'center',
+  },
+  swipeableContainer:{
+    width: '100%',
+    height: 90,
+    marginBottom: 12,
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    borderRadius:6,
+  },
 });
